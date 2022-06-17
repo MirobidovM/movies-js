f7047989b51c7890759fa1ab5013d1e0eae621d6
MirobidovM <miriliyosmiriobidov@gmail.com>
2022-06-17 21:16:16 +0500

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,24 +1,25 @@
-let wrapper = findEl(".row");
-let btn = findEl(".btn");
+let wrapper = findEl('.row');
+let btn = findEl('.btn');
 let n = 0;
 let m = 7;
 
-btn.addEventListener("click", watchMore);
+btn.addEventListener('click', watchMore);
+
 function watchMore() {
   wrapper.innerHTML = null;
   for (let i = n; i <= m; i++) {
     // creating elements
 
-    let elCard = createEl("div");
-    let elCardImg = createEl("img");
-    let elCardWrap = createEl("div");
-    let elCardTitle = createEl("h3");
-    let elCardText = createEl("p");
-    let elCardList = createEl("ul");
-    let elCardItem = createEl("li");
-    let elCardItem2 = createEl("li");
-    let elCardItem3 = createEl("li");
-    let elCardLink = createEl("a");
+    let elCard = createEl('div');
+    let elCardImg = createEl('img');
+    let elCardWrap = createEl('div');
+    let elCardTitle = createEl('h3');
+    let elCardText = createEl('p');
+    let elCardList = createEl('ul');
+    let elCardItem = createEl('li');
+    let elCardItem2 = createEl('li');
+    let elCardItem3 = createEl('li');
+    let elCardLink = createEl('a');
 
     // appending elements
 
@@ -35,17 +36,17 @@ function watchMore() {
 
     //giving Classnames
 
-    elCard.className = "card";
-    elCardImg.className = "card-img-top";
-    elCardWrap.className = "card-body";
-    elCardTitle.className = "card-title";
-    elCardText.className = "card-text";
-    elCardList.className = "list-group list-group";
-    elCardItem.className = "list-group-item";
-    elCardItem2.className = "list-group-item";
-    elCardItem3.className = "list-group-item";
+    elCard.className = 'card';
+    elCardImg.className = 'card-img-top';
+    elCardWrap.className = 'card-body';
+    elCardTitle.className = 'card-title';
+    elCardText.className = 'card-text';
+    elCardList.className = 'list-group list-group';
+    elCardItem.className = 'list-group-item';
+    elCardItem2.className = 'list-group-item';
+    elCardItem3.className = 'list-group-item';
     elCardLink.className =
-      "btn btn-secondary my-2 text-light mx-auto card-link";
+      'btn btn-secondary my-2 text-light mx-auto card-link';
 
     //Rendering
 
@@ -55,15 +56,15 @@ function watchMore() {
     elCardItem.textContent = movies[i].language;
     elCardItem2.textContent = movies[i].imdbRating;
     elCardItem3.textContent = movies[i].runtime;
-    elCardLink.textContent = "Trailer >>";
+    elCardLink.textContent = 'Trailer >>';
 
     // Style
 
-    elCard.style.width = "17rem";
+    elCard.style.width = '17rem';
     elCardLink.href = movies[i].trailer;
-    elCardLink.style.cursor = "pointer";
-    elCardLink.style.color = "black";
-    elCardLink.style.textDecoration = "none";
+    elCardLink.style.cursor = 'pointer';
+    elCardLink.style.color = 'black';
+    elCardLink.style.textDecoration = 'none';
   }
 
   n += 8;
